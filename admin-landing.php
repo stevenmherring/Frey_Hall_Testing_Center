bb0a2ea1daf51ef9771c48d4ed05415318b26267
ChrisJohnRyan <chrisjohnryan@live.com>
2015-09-24 13:37:04 -0400

    Modified admin-landing.php

--- admin-landing.php
+++ admin-landing.php
@@ -1,8 +1,14 @@
 <h3>Admin landing page</h3>
 
 <button onclick="checkInStudent()">Check in</button>
+<button onclick="scheduleStudent()">Schedule Student</button>
 
 <script>
 function checkInStudent() {
+    /* To Do*/
+}
+
+<script>
+function scheduleStudent() {
     /* To Do*/
 }
