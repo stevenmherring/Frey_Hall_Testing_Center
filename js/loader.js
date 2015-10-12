adafaa526afeee1b23f68362f3dcc850487e6d81
ChrisJohnRyan <chrisjohnryan@live.com>
2015-10-11 20:20:33 -0400

    Added in fnav loading loader.js after mistaken removal.

--- js/loader.js
+++ js/loader.js
@@ -16,6 +16,13 @@ $('ul#anav li a').click(function(){
   return false;
 });
 
+$('ul#fnav li a').click(function(){
+  var page = $(this).attr('href');
+  $('#fcontent').load(page)
+
+  return false;
+});
+
 $(document).ready(function (){
   $("#fcontent").load("faculty-landing.php");
 });
