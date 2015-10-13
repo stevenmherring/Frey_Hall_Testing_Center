81b1db78edf3a6d89ffa535bb0e18df66c977fa6
ChrisJohnRyan <chrisjohnryan@live.com>
2015-10-12 20:14:30 -0400

    Insert ignore added to initialize a room row in the database if not existing under name freyhalltesting.

--- adminEditCenter-form-validation.php
+++ adminEditCenter-form-validation.php
@@ -5,64 +5,57 @@
   <h3>Form submitted</h3>
 
 <?php
-include("dbQueries.php");
-//error_reporting(E_ALL);
-//ini_set('display_errors', 'On');
-/*
-        var inputSetasideSeats = document.forms["adminEditCenterForm"]["inputSetasideSeats"].value;
-            var datePick_RangeClosedFrom = document.forms["adminEditCenterForm"]["datePick_RangeClosedFrom"].value;
-                var datePick_RangeClosedTo = document.forms["adminEditCenterForm"]["datePick_RangeClosedTo"].value;
-                    var datePick_ReservedTimePeriodFrom = document.forms["adminEditCenterForm"]["datePick_ReservedTimePeriodFrom"].value;
-                        var datePick_ReservedTimePeriodTo = document.forms["adminEditCenterForm"]["datePick_ReservedTimePeriodTo"].value;
-                            var reminderChoice = document.forms["adminEditCenterForm"]["reminderChoice"].value;
-*/
+  include("dbQueries.php");
+  include("editcenterfunctions.php");
+  error_reporting(E_ALL);
+  ini_set('display_errors', 'On');
+  try {
+         $dbh=new PDO("mysql:host=localhost;dbname=FreyHallTestingCenter","root","physics123");
+  } catch(PDOException $e) {
 
-try {
-       $dbh=new PDO("mysql:host=localhost;dbname=FreyHallTestingCenter","root","physics123");
-} catch(PDOException $e) {
+    $message = "Couldnt connect to db.";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+  }
 
-  $message = "Couldnt connect to db.";
-  echo "<script type='text/javascript'>alert('$message');</script>";
-}
+  $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+  /**************************************************************************
+  *********************Getting all of the form information*******************
+  /* Grabbing the form value of numberOfSeats from our adminEditCenterForm */
+  $numseats = $_POST['numberOfSeats'];
+  /*Strip tags- This function tries to return a string with all NULL bytes, HTML and PHP tags stripped from a given str.
+  It uses the same tag stripping state machine as the fgetss() function.*/
+  $numseats = strip_tags($numseats);
 
-/**************************************************************************
-*********************Getting all of the form information*******************
-/* Grabbing the form value of numberOfSeats from our adminEditCenterForm */
-$numseats = $_POST['numberOfSeats'];
-/*Strip tags- This function tries to return a string with all NULL bytes, HTML and PHP tags stripped from a given str.
-It uses the same tag stripping state machine as the fgetss() function.*/
-$numseats = strip_tags($numseats);
+  $numsetasideseats = $_POST['inputSetasideSeats'];
+  $numsetasideseats = strip_tags($numsetasideseats);
 
-$numsetasideseats = $_POST['inputSeatasideSeats'];
-$numsetasideseats = strip_tags($numseats);
+  $hours_openfrom = $_POST['hours_openfrom'];
+  $hours_openfrom = strip_tags($hours_openfrom);
 
-$hours_openfrom = $_POST['hours_openfrom'];
-$hours_openfrom = strip_tags($hours_openfrom);
+  $hours_openuntil = $_POST['hours_openuntil'];
+  $hours_openuntil = strip_tags($hours_openuntil);
 
-$hours_openuntil = $_POST['hours_openuntil'];
-$hours_openuntil = strip_tags($hours_openuntil);
+  $gaptime = $_POST['gaptime'];
+  $gaptime = strip_tags($gaptime);
 
-$gaptime = $_POST['gaptime'];
-$gaptime = strip_tags($gaptime);
+  /* to do these fields are not defined in the XML
+  $datePick_RangeClosedFrom = $_POST['datePick_RangeClosedFrom'];
+  $datePick_RangeClosedTo = $_POST['datePick_RangeClosedTo'];
+  $datePick_ReservedTimePeriodFrom = $_POST['datePick_ReservedTimePeriodFrom'];
+  $datePick_ReservedTimePeriodTo = $_POST['datePick_ReservedTimePeriodTo'];
+  */
 
-/* to do these fields are not defined in the XML
-$datePick_RangeClosedFrom = $_POST['datePick_RangeClosedFrom'];
-$datePick_RangeClosedTo = $_POST['datePick_RangeClosedTo'];
-$datePick_ReservedTimePeriodFrom = $_POST['datePick_ReservedTimePeriodFrom'];
-$datePick_ReservedTimePeriodTo = $_POST['datePick_ReservedTimePeriodTo'];
-*/
+  $dbh->beginTransaction();
 
-$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-$dbh->beginTransaction();
+  $roomname = "freyhalltesting";
+  initializeRoom($roomname);
+  $query = $q_edit_seats;
+  $result = $dbh->prepare($query);
 
-$roomInfo;
-$q_edit_seatsHard = "update freyHallTestingCenterRoom set numseats = '{$numseats}';";
 
-$query = $q_edit_seatsHard;
-$result = $dbh->prepare($query);
 
-echo "<script type='text/javascript'>alert('$numseats');</script>";
-$result->execute();
+  echo "<script type='text/javascript'>alert('$numseats');</script>";
+  $result->execute(array($numseats));
 /*
 $query = $q_view_room;
 $result = $dbh->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
@@ -71,6 +64,21 @@ $result->execute(array($roomInfo));
 */
   $dbh->commit();
   $dbh = null;
+
+  function initializeRoom($roomname) {
+
+    $createRowQuery = $q_createrow_testingcenterroom;
+    $createRowsuccess = $dbh->prepare($query);
+    $createRowsuccess->execute(array($roomname));
+    if (!$createRowsuccess){
+      $error = "Something went wrong. Rolling back.";
+      echo "<script type='text/javascript'>alert('$message');</script>";
+      $dbh->rollback();
+      $dbh = null;
+      return;
+    }
+}
+
 ?>
 </body>
 </html>
