bc73fb55fb4759d91779960d889f3bbc8d59de08
steve <StevenMHerring@gmail.com>
2015-10-13 19:29:22 -0400

    made logging for successful and failed logins

--- includes/header.php
+++ includes/header.php
@@ -17,7 +17,7 @@
         </div>
         <!-- nav links, forms, and other content for toggling -->
 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"> 
             <div>
             <ul class="nav navbar-nav navbar-right">
                 <li>
