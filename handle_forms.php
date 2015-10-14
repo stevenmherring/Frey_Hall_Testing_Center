053040d094678d54dec17d2e67aa758252ecc513
steve <StevenMHerring@gmail.com>
2015-10-13 23:36:17 -0400

    steven chin changes

--- handle_forms.php
+++ handle_forms.php
@@ -1,89 +1,153 @@
-<?php
-$servername = "mysql2.cs.stonybrook.edu";
-$username = "sachin";
-$password = "108610059";
-$dbname = "sachin";
+<?php 
+include("dbQueries.php");
+//error_reporting(E_ALL);
+//ini_set('display_errors', 'On');
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+       $dbh=new PDO("mysql:host=mysql2.cs.stonybrook.edu;dbname=sachin","sachin","108610059");
+} catch(PDOException $e) {
+  $message = "Couldnt connect to db.";
+  echo "<script type='text/javascript'>alert('$message');</script>";
 }
 $userRow=0;
 $rosterRow=0;
 $classRow=0;
-if ($_POST[user_form]!=null){
-    if (($handle = fopen("$_POST[user_form]", "r")) !== FALSE) {
+$dbh->beginTransaction();
+$userForm = $_POST['user_form'];
+$rosterForm = $_POST['roster_form'];
+$classForm = $_POST['class_form'];
+
+if ($userForm!=null){
+    if (($handle = fopen("$userForm", "r")) !== FALSE) {
         $data = fgetcsv($handle, 1000, ",");
         $sql = "DELETE FROM user";
-        $conn->query($sql);
+        $result = $dbh->prepare($sql);
+        if (!$result){
+          $prepareFail = "Information NOT updated.";
+          echo "<script type='text/javascript'>alert('$prepareFail');</script>";
+          $dbh->rollback();
+          $dbh = null;
+          return;
+        }
+        //$conn->query($sql);
+        $result->execute();
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
             $num = count($data);
             if ($data[2]!=null){
                 $sql = "INSERT INTO user(firstName, lastName, netID, email)
                 VALUES ('$data[0]', '$data[1]', '$data[2]', '$data[3]')";
-                if ($conn->query($sql) === TRUE) {
-                    $userRow++;
+                $result = $dbh->prepare($sql);
+                /*if ($conn->query($sql) === TRUE) {}*/
+                if ($result->execute() === TRUE){
+                  $stmtCSVlog = $transactionLogging;
+                  $stmtCSVquery = $dbh->prepare($stmtCSVlog);
+                  $transactionContent = "classID:" . $data[0] . "," . "subj:" . $data[1] . "," . "section:" . $data[2] .  "," . "catalogNumber:" . $data[3] .  "," . "instructorNetID:" . $data[4]  . ",";
+                  $transactionType = "importcsv";
+                  $now = time();
+                  $userID = "placeholder";
+                  //"INSERT INTO transactionlog_tbl(userID,transactiontype,transactiontime,transactioncontent)VALUES(?,?,?,?)";
+                  $stmtCSVquery->execute(array($userID,$transactionType,$now,$transactionContent));
+                  $userRow++;
                 }
+
+
             }
         }
-        fclose($handle);
+        
     }
-} else{
-      $errUpdate = "chin fucked up";
-      echo "<script type='text/javascript'>alert('$errUpdate');</script>";
+    
+    fclose($handle);
 }
-if ($_POST[roster_form]!=null){
-    if (($handle = fopen("$_POST[roster_form]", "r")) !== FALSE) {
+
+if ($rosterForm!=null){
+    if (($handle = fopen($rosterForm, "r")) !== FALSE) {
         $data = fgetcsv($handle, 1000, ",");
         $sql = "DELETE FROM roster";
-        $conn->query($sql);
+        $result = $dbh->prepare($sql);
+        if (!$result){
+          $prepareFail = "Information NOT updated.";
+          echo "<script type='text/javascript'>alert('$prepareFail');</script>";
+          $dbh->rollback();
+          $dbh = null;
+          return;
+        }
+        //$conn->query($sql);
+        $result->execute();
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-            $num = count($data);
-            if ($data[0] != null and $data[1] != null){
-                $sql = "INSERT INTO roster(netID, classID)
-                VALUES ('$data[0]', '$data[1]')";
-                if ($conn->query($sql) === TRUE) {
-                    $rosterRow++;
+            if ($data[1]!=null){
+                $sql = "INSERT INTO roster(netID, classID) VALUES ('$data[0]', '$data[1]')";
+                $result = $dbh->prepare($sql);
+                /*if ($conn->query($sql) === TRUE) {}*/
+                if ($result->execute() === TRUE){
+                    echo "$rosterRow";
+                
+                    $stmtCSVlog = $transactionLogging;
+                  $stmtCSVquery = $dbh->prepare($stmtCSVlog);
+                  $transactionContent = "netID:" . $data[0] . "," . "classID:" . $data[1] . ",";
+                  $transactionType = "importcsv";
+                  $now = time();
+                  $userID = "placeholder";
+                  //"INSERT INTO transactionlog_tbl(userID,transactiontype,transactiontime,transactioncontent)VALUES(?,?,?,?)";
+                  $stmtCSVquery->execute(array($userID,$transactionType,$now,$transactionContent));
+                  $rosterRow++;
                 }
+
+
             }
         }
-        fclose($handle);
+        
     }
-}else{
-      $errUpdate = "chin fucked up";
-      echo "<script type='text/javascript'>alert('$errUpdate');</script>";
+    fclose($handle);
 }
-if ($_POST[class_form]!=null){
-    if (($handle = fopen("$_POST[class_form]", "r")) !== FALSE) {
+
+if ($classForm!=null){
+    if (($handle = fopen("$classForm", "r")) !== FALSE) {
         $data = fgetcsv($handle, 1000, ",");
         $sql = "DELETE FROM class";
-        $conn->query($sql);
+        $result = $dbh->prepare($sql);
+        if (!$result){
+          $prepareFail = "Information NOT updated.";
+          echo "<script type='text/javascript'>alert('$prepareFail');</script>";
+          $dbh->rollback();
+          $dbh = null;
+          return;
+        }
+        //$conn->query($sql);
+        $result->execute();
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
             $num = count($data);
-            if ($data[0]!= null){
-                $sql = "INSERT INTO class(classID, subj, catalogNumber, section, instructorNetID)
-                VALUES ('$data[0]', '$data[1]', $data[2], '$data[3]','$data[4]')";
-                if ($conn->query($sql) === TRUE) {
-                    $classRow++;
+            if ($data[0]!=null){
+                $sql = "INSERT INTO class(classID, subj, section, instructorNetID)VALUES('$data[0]', '$data[1]','$data[2]', '$data[3]' )";
+                $result = $dbh->prepare($sql);
+                /*if ($conn->query($sql) === TRUE) {}*/
+                if ($result->execute() === TRUE){
+                  $stmtCSVlog = $transactionLogging;
+                  $stmtCSVquery = $dbh->prepare($stmtCSVlog);
+                  $transactionContent = "classID:" . $data[0] . "," . "subj:" . $data[1] . "," . "section:" . $data[2] .  "," . "instructorNetID:" . $data[3] ;
+                  $transactionType = "importcsv";
+                  $now = time();
+                  $userID = "placeholder";
+                  //"INSERT INTO transactionlog_tbl(userID,transactiontype,transactiontime,transactioncontent)VALUES(?,?,?,?)";
+                  $stmtCSVquery->execute(array($userID,$transactionType,$now,$transactionContent));
+                  $userRow++;
                 }
+
+
             }
         }
         fclose($handle);
     }
 }
 
-
+if ($userRow==0 and $rosterRow==0 and $classRow==0){
+?> <h3>Import Failed or not files have been submitted</h3><?php
+}
+else{
 ?>
      <h3> Import Successful</h3>
      <p> You have sucessfully imported <?php echo $userRow ?> user rows, <?php echo $rosterRow ?> roster rows, and <?php echo $classRow ?> class rows for the <?php
     echo $_POST[term];?> term.</p>
-     <button onclick="goBack()">Go Back</button>
 
-
-
-
-<?php
-$conn->close();
-?>
+<?php }  
+        $dbh->commit();
+        $dbh=null;?>
