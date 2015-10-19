915e8a793acddc640cb291bd58895fd4d1702c08
shilinlu <shilinlu123@yahoo.com>
2015-10-18 22:02:28 -0400

    fixed errors. up to date with master

--- createExam.php
+++ createExam.php
@@ -46,6 +46,98 @@
                 }
 ?>
 
+<?php
+$servername = "mysql2.cs.stonybrook.edu";
+$username = "sachin";
+$password = "108610059";
+$dbname = "sachin";
+// retrieves user input from textbox
+if(! empty($_POST['examID'])){
+    $examIDs = $_POST['examID'];
+	//echo $examIDs;
+	}
+if(! empty($_POST['catalogueNumber'])){
+    $catalogueNumbers = $_POST['catalogueNumber'];
+	//echo $courseIDs;
+	}
+if(! empty($_POST['subj'])){
+    $subjs = $_POST['subj'];
+	//echo $professorIDs;
+	}	
+	
+if(! empty($_POST['professorNetID'])){
+    $professorNetIDs = $_POST['professorNetID'];
+	//echo $professorIDs;
+	}	
+if(! empty($_POST['examDuration'])){
+    $examDurations = $_POST['examDuration'];
+	//echo $examDurations;
+	}
+if(! empty($_POST['startDate'])){
+    $startDates = $_POST['startDate'];
+	//echo $startDates;
+	}	
+if(! empty($_POST['endDate'])){
+    $endDates = $_POST['endDate'];
+	//echo $endDates;
+	}	
+if(! empty($_POST['startTime'])){
+    $startTimes = $_POST['startTime'];
+	//echo $startTimes;
+	}	
+if(! empty($_POST['endTime'])){
+    $endTimes = $_POST['endTime'];
+	//echo $endTimes;
+	}		
+
+	
+	// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sqlb = "SELECT numseats FROM freyhalltestingcenterroom";
+$resultx = $conn->query($sqlb);
+$numseats = mysqli_fetch_assoc($resultx);   // gets the available number of seats from freyhalltestingcenterroom table
+echo "Available Seats: " . $numseats["numseats"];
+
+if($numseats>0){
+$sqb = "INSERT INTO exam (examID, catalogueNumber,subj,professorNetID,examStartDate,examEndDate,examStartTime,examEndTime,examDuration)
+VALUES ('$examIDs', '$catalogueNumbers','$subjs','$professorNetIDs','$startDates','$endDates','$startTimes','$endTimes','$examDurations')";
+
+$final=$conn->query($sqb);   // if there is available seats
+}
+if($numseats<0){
+	echo "<div style ='font:30px Arial,tahoma,sans-serif;color:#ff0000;text-align:center;'> Error! No available seats</div>";
+}
+
+if($final){
+	echo "<div style ='font:30px Arial,tahoma,sans-serif;color:#00FF00;text-align:center;'> Success! Exam Scheduled</div>";
+}
+else {
+	echo "<div style ='font:30px Arial,tahoma,sans-serif;color:#ff0000;text-align:center;'> Error! ExamID already in use</div>";
+}
+?>
+
+<br><br><br>
+
+<?php
+$sql = "SELECT examID,catalogueNumber,subj,professorNetID,examStartDate,examEndDate,examStartTime,examEndTime,examDuration,processed FROM exam";
+$result = mysqli_query($conn, $sql);   
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "ExamID: " . $row["examID"]."&nbsp;&nbsp; Catalogue Number: " . $row["catalogueNumber"]."&nbsp;&nbsp; Subject: " . $row["subj"]. "&nbsp;&nbsp;&nbsp;&nbsp;Professor NetID: " . $row["professorNetID"]. "&nbsp;&nbsp;&nbsp;&nbsp;Start Date: " . $row["examStartDate"]. "&nbsp;&nbsp;&nbsp;&nbsp;Start Time: " . $row["examStartTime"]."&nbsp;&nbsp;&nbsp;&nbsp;End Date: ". $row["examEndDate"]. "&nbsp;&nbsp;&nbsp;&nbsp;End Time: " . $row["examEndTime"]. "&nbsp;&nbsp;&nbsp;&nbsp;Duration: " . $row["examDuration"]."&nbsp;&nbsp;&nbsp;&nbsp;Processed: " . $row["processed"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
 
 <!-- jQuery -->
 <script src="js/jquery.js"></script>
