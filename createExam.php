4572b53e47c79cce4730520319fe1801ea1688f4
stevechin <stevechin13@gmail.com>
2015-10-27 03:30:36 -0400

    Finished create exam with all error check i can think  (no class tho)

--- createExam.php
+++ createExam.php
@@ -1,46 +1,43 @@
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>Frey Hall Testing Center at Stony Brook University</title>
-  <!-- Bootstrap Core CSS -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <!-- Custom CSS -->
-  <link href="css/landing-page.css" rel="stylesheet">
-  <!-- Custom Fonts -->
-  <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-  <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
-  <script type="text/JavaScript" src="js/sha512.js"></script>
-  <script type="text/JavaScript" src="js/forms.js"></script>
-</head>
-<body>
 <?php
-        if (Authentication::login_check($db->getMysqli() == true) {
-            if ($_SESSION['auth'] == 0) {
-              // auth level 0 ADMIN
-              header('Location: access-error.php');
-            } else if($_SESSION['auth'] == 1) {
-              // auth level 1 INSTRUCTOR
-              echo file_get_contents('processExam.php');
-            } else if($_SESSION['auth'] == 2) {
-               //auth level 2 STUDENT
-               header('Location: access-error.php');
-             }
+include("dbQueries.php");
+//error_reporting(E_ALL);
+//ini_set('display_errors', 'On');
 
-        } else {
-                        header('Location: access-error.php');
-                }
+try {
+       $dbh=new PDO("mysql:host=mysql2.cs.stonybrook.edu;dbname=sachin","sachin","108610059");
+} catch(PDOException $e) {
+  $message = "Couldnt connect to db.";
+  echo "<script type='text/javascript'>alert('$message');</script>";
+}
+$dbh->beginTransaction();
+$examName = $_POST['examName'];
+$examDuration = $_POST['examDuration'];
+$examStartDate = $_POST['startDate'];
+$examEndDate = $_POST['endDate'];
+$examStartHr = $_POST['startHr'];
+$examStartMin = $_POST['startMin'];
+$examEndHr = $_POST['endHr'];
+$examEndMin = $_POST['endMin'];
+
+$examStartTime = $examStartHr . ":" . $examStartMin . ":" . "00";
+$examEndTime = $examEndHr . ":" . $examEndMin . ":" . "00";
+
+echo $examStartTime;
+echo $examEndTime;
+
+$sql = "INSERT INTO exam(examStartDate, examEndDate, examStartTime, examEndTime, examDuration, examName, classID) VALUES ('$examStartDate', '$examEndDate', '$examStartTime', '$examEndTime', $examDuration, '$examName', '11111-1111')";
+        $result = $dbh->prepare($sql);
+        if (!$result){
+          $prepareFail = "Information NOT updated.";
+          echo "<script type='text/javascript'>alert('$prepareFail');</script>";
+          $dbh->rollback();
+          $dbh = null;
+          return;
+        }
+        //$conn->query($sql);
+$result->execute();
+echo "Exam Created";
+
+$dbh->commit();
+        $dbh=null;
 ?>
-
-
-<!-- jQuery -->
-<script src="js/jquery.js"></script>
-
-<!-- Bootstrap Core JavaScript -->
-<script src="js/bootstrap.min.js"></script>
-<script type="text/javascript"></script>
-
-</body>
